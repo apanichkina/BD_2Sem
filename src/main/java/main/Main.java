8c114f5fb49037815b057814dfabfae3699a66c0
anna <annjellyiu5@gmail.com>
2015-10-20 19:05:27 +0300

    UTF-8

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
 
     public static final int  STANDARTPORT = 8080;
-    public static final String URL_DB = "jdbc:mysql://localhost:3306/forumdb";
+    public static final String URL_DB = "jdbc:mysql://localhost:3306/forumdb?autoreconnect=true&useUnicode=yes&characterEncoding=UTF-8";
     public static final String USER_DB = "root";
     public static final String PASSWORD_DB = "12345";
 
